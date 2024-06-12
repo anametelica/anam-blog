f0525d053df78166b3fee12d8dfdc3d0d3abbe95
Anastasija <anastasijametelica@gmail.com>
2024-06-12 19:52:22 +0100

    adding logo

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Ana's Web Development Blog",
-	url: "https://example.com/",
+	url: "https://main--anametelica.netlify.app/",
 	language: "en",
-	description: "I am writing about my journey of Web Development.",
+	description: "I am writing about my journey to becoming a Web Developer.",
 	author: {
 		name: "Anastasija Metelica",
 		email: "anastasijametelica@gmail.com",
-		url: "https://example.com/about-me/"
+		url: "https://main--anametelica.netlify.app/about/"
 	}
 }
